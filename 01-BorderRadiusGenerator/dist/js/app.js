7dbbd4aa2b95e3b532ed067768bda483a14f5a60
Dima <webbootcamp2020@gmail.com>
2021-01-11 22:57:40 +0200

    Added border style inputs, css code preveiw with copy to clipboard functionality

--- 01-BorderRadiusGenerator/dist/js/app.js
+++ 01-BorderRadiusGenerator/dist/js/app.js
@@ -5,6 +5,10 @@ const previewContainerBgInput = document.getElementById("container-bg");
 const previewContainerBgHexInput = document.getElementById("container-bg-hex");
 const borderWidthInput = document.getElementById("border-width-input");
 const radiusAllInput = document.getElementById("radius-all");
+const borderStyleInput = document.getElementById("border-style-input");
+const borderColorInput = document.getElementById("border-color-input");
+const borderColorHexInput = document.getElementById("border-color-hex");
+const borderInputs = document.querySelectorAll("[data-border-change]");
 
 const previewBoxEl = document.querySelector(".generator__preview-box");
 const previewContainerEl = document.querySelector(
@@ -12,8 +16,12 @@ const previewContainerEl = document.querySelector(
 );
 const borderRadiusNumberEl = document.getElementById("radius-number");
 const borderWidthNumberEl = document.getElementById("border-width-number");
+const copyCodeEl = document.querySelector(".generator__code");
+const copyBorderRadiusTextEl = document.getElementById("radius-text");
+const copyBorderStyleTextEl = document.getElementById("border-width-text");
 
 let arrowDeg = 0;
+let borderPrefix = "px";
 
 const changeBg = (element, color) => {
   element.style.backgroundColor = color;
@@ -23,20 +31,54 @@ const syncColorAndTextInputs = (input, value) => {
   input.value = value;
 };
 
-const syncRangeAndNumPreview = (element, value) => {
+const updateNumDisplay = (element, value) => {
   element.innerText = `${value}px`;
 };
 
-const changeBorderWidth = (e) => {
-  const width = e.target.value;
-  previewBoxEl.style.borderWidth = `${width}px`;
-  syncRangeAndNumPreview(borderWidthNumberEl, width);
+const updateBorderStyles = (e) => {
+  const input = e.target;
+  const method = input.dataset.borderChange;
+  borderPrefix = input.id === "border-width-input" ? "px" : "";
+  previewBoxEl.style[method] = input.value + borderPrefix;
+  updateCopyText(e);
+  if (method === "borderWidth") {
+    updateNumDisplay(borderWidthNumberEl, input.value);
+  } else if (method === "borderColor") {
+    syncColorAndTextInputs(borderColorHexInput, input.value);
+  }
+};
+
+const updateInnerText = (
+  element,
+  width,
+  style = "solid",
+  color = "#800000"
+) => {
+  const text = `${width}px ${style} ${color}`;
+  element.innerText = text;
 };
 
 const updateBorderRadius = (e) => {
   const radius = e.target.value;
   previewBoxEl.style.borderRadius = `${radius}px`;
-  syncRangeAndNumPreview(borderRadiusNumberEl, radius);
+  updateNumDisplay(borderRadiusNumberEl, radius);
+  updateNumDisplay(copyBorderRadiusTextEl, radius);
+};
+
+const updateCopyText = (e) => {
+  const borderWidth = borderWidthInput.value;
+  const borderStyle = borderStyleInput.value;
+  const borderColor = borderColorInput.value;
+  updateInnerText(copyBorderStyleTextEl, borderWidth, borderStyle, borderColor);
+};
+
+const copyToClipBoard = (selector) => {
+  const r = document.createRange();
+  r.selectNode(document.querySelector(selector));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
 };
 
 const openCloseControll = (e) => {
@@ -80,11 +122,22 @@ previewContainerBgHexInput.addEventListener("input", (e) => {
 });
 
 // Border Inputs Event Listeners
-borderWidthInput.addEventListener("mousemove", changeBorderWidth);
-borderWidthInput.addEventListener("change", changeBorderWidth);
+borderInputs.forEach((input) => {
+  input.addEventListener("mousemove", updateBorderStyles);
+  input.addEventListener("change", updateBorderStyles);
+});
 
 radiusAllInput.addEventListener("mousemove", updateBorderRadius);
 radiusAllInput.addEventListener("change", updateBorderRadius);
+
+copyCodeEl.addEventListener("click", () => {
+  const tip = copyCodeEl.querySelector(".generator__code-tip");
+  tip.classList.add("clicked");
+  setTimeout(() => {
+    tip.classList.remove("clicked");
+  }, 500);
+  copyToClipBoard("#code-box");
+});
 
 controllTriggers.forEach((trigger) => {
   trigger.addEventListener("click", openCloseControll);
