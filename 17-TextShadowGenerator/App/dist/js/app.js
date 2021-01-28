a4233fea524a90045f548c1cd4eac966d4ea4d2b
Dima <webbootcamp2020@gmail.com>
2021-01-28 23:09:48 +0200

    added multy text-shadow functionality

--- 17-TextShadowGenerator/App/dist/js/app.js
+++ 17-TextShadowGenerator/App/dist/js/app.js
@@ -1,43 +1,55 @@
-const previewTextEl = document.getElementById('preview-text');
-const previewContainer = document.getElementById('preview-container');
+const DEFAULT_SHADOW = {
+  id: 1,
+  hLen: 3,
+  vLen: 3,
+  blurRadius: 2,
+  opacity: 1,
+  shadowColor: "#A82727",
+};
 
-const textInputs = document.querySelectorAll('.text-inputs');
+const previewTextEl = document.getElementById("preview-text");
+const previewContainer = document.getElementById("preview-container");
 
-const shadowInputs = document.querySelectorAll('.shadow-input');
+const textInputs = document.querySelectorAll(".text-inputs");
 
-const textColorInput = document.getElementById('text-color');
-const textColorHexInput = document.getElementById('text-color-hex');
+const shadowInputs = document.querySelectorAll(".shadow-input");
 
-const bgColorInput = document.getElementById('bg-color');
-const bgColorHexInput = document.getElementById('bg-color-hex');
+const textColorInput = document.getElementById("text-color");
+const textColorHexInput = document.getElementById("text-color-hex");
 
-const shadowOpacityInput = document.getElementById('opacity');
+const bgColorInput = document.getElementById("bg-color");
+const bgColorHexInput = document.getElementById("bg-color-hex");
 
-const addShadowBtn = document.getElementById('add-shadow');
+const shadowOpacityInput = document.getElementById("opacity");
+
+const addShadowBtn = document.getElementById("add-shadow");
 
 let shadowCounter = 2;
 
+const shadows = [DEFAULT_SHADOW];
+
 // Functions
 
+// Helper Functions
+
+const getRandomNumber = (min, max) => {
+  // 5, 10
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const syncInputs = (value, firstInput, secondInput) => {
-    firstInput.value = value;
-    secondInput.value = value;
-}
+  firstInput.value = value;
+  secondInput.value = value;
+};
 
 const syncInputAndText = (value, textEl) => {
-    textEl.textContent = value;
-}
-
-const changePreviewText = (value) => {
-    previewTextEl.style.color = value;
-}
-
-const changePreviewContainerBg = (value) => {
-    previewContainer.style.backgroundColor = value;
-}
+  textEl.textContent = value;
+};
 
 const hexToRgba = (h, op) => {
-    let r = 0, g = 0, b = 0;
+  let r = 0,
+    g = 0,
+    b = 0;
 
   // 3 digits
   if (h.length == 4) {
@@ -45,7 +57,7 @@ const hexToRgba = (h, op) => {
     g = "0x" + h[2] + h[2];
     b = "0x" + h[3] + h[3];
 
-  // 6 digits
+    // 6 digits
   } else if (h.length == 7) {
     r = "0x" + h[1] + h[2];
     g = "0x" + h[3] + h[4];
@@ -53,108 +65,200 @@ const hexToRgba = (h, op) => {
   }
 
   return "rgba(" + +r + "," + +g + "," + +b + "," + op + ")";
-}
+};
 
+const RGBToHex = (r, g, b) => {
+  r = r.toString(16);
+  g = g.toString(16);
+  b = b.toString(16);
 
-const textInputHandler = e => {
-    const value = e.target.value;
-    const type = e.target.dataset.type === 'text-color' ? changePreviewText(value) : changePreviewContainerBg(value);
-    syncInputs(value, textColorInput, textColorHexInput);
-    
-}
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
 
+  return "#" + r + g + b;
+};
 
+// Preview Functions
+const changePreviewText = (value) => {
+  previewTextEl.style.color = value;
+};
 
-const getTextShadowString = () => {
-    let str = '';
-    shadowInputs.forEach(input => {
-        if (input.type === 'range' && input.id !== 'opacity') {
-            str += `${input.value}px `
-        } else if (input.type === 'color') {
-            str += `${hexToRgba(input.value, shadowOpacityInput.value)}`;
-        }
-    });
-    //console.log(str);
-    return str;
-}
+const changePreviewContainerBg = (value) => {
+  previewContainer.style.backgroundColor = value;
+};
 
-const updateCode = () => {
-    document.getElementById('code-string').textContent = `text-shadow: ${getTextShadowString()};`;
-}
+const createShadowObject = () => {
+  const shadowObj = {
+    id: shadowCounter,
+    hLen: getRandomNumber(-20, 20),
+    vLen: getRandomNumber(-20, 20),
+    blurRadius: getRandomNumber(0, 25),
+    opacity: getRandomNumber(0, 100) / 100,
+    shadowColor: RGBToHex(
+      getRandomNumber(0, 255),
+      getRandomNumber(0, 255),
+      getRandomNumber(0, 255)
+    ),
+  };
+  shadows.push(shadowObj);
+  console.log(shadows);
+  return shadowObj;
+};
 
-const updateTextShadow = () => {
-    previewTextEl.style.textShadow = `${getTextShadowString()}`;
-    updateCode();
-}
+const checkIfOneShadow = () => {
+  const firstShadow = document.querySelector(".shadows-list li.first-shadow");
+  if (firstShadow.nextElementSibling) firstShadow.classList.add("active");
+};
 
-const changeShadowHandler = e => {
-    const type = e.target.type;
-    if (type === 'range') {
-        // Querying for range value preview el
-        const inputValuePreviewEl = e.target.previousElementSibling.querySelector('span');
-        syncInputAndText(e.target.value, inputValuePreviewEl);
-    } else if (type === 'color') {
-        syncInputs(e.target.value, e.target, e.target.nextElementSibling);
-    } else if (type === 'text') {
-        syncInputs(e.target.value, e.target, e.target.previousElementSibling);
+const removeShadowLi = (e) => {
+  const shadowToRemove = e.target.closest("li");
+  const shadowId = +shadowToRemove.querySelector("span").dataset.shnum - 1;
+
+  shadows.splice(shadowId, 1);
+  for (let i = shadowId; i < shadows.length; i++) {
+    shadows[i].id -= 1;
+  }
+  updateCode(updateTextShadow());
+
+  let nextShadow = shadowToRemove.nextElementSibling;
+  // If next el has nex-shadow class than update id, content, data-attr;
+  while (nextShadow.classList.contains("new-shadow")) {
+    const shadowIdEl = nextShadow.querySelector("span[data-shnum]");
+    // Get shadow id number and decrease by 1;
+    const shadowIdNum = +shadowIdEl.id.split("-")[1] - 1;
+    shadowIdEl.id = `shadow-${shadowIdNum}`;
+    shadowIdEl.textContent = shadowIdNum;
+    shadowIdEl.dataset.shnum = shadowIdNum;
+    nextShadow = nextShadow.nextElementSibling;
+  }
+
+  if (shadowToRemove.classList.contains("active")) {
+    shadowToRemove.previousElementSibling.classList.add("active");
+  }
+  shadowToRemove.remove();
+  shadowCounter--;
+};
+
+const updateNewShadowInputs = (obj) => {
+  const { omit, ...shadowProps } = obj;
+  shadowInputs.forEach((input, idx) => {
+    if (input.type === "range") {
+      const textPreview = input.previousElementSibling.querySelector("span");
+      input.value = shadowProps[input.dataset.type];
+      syncInputAndText(input.value, textPreview);
+    } else if (input.type === "color") {
+      input.value = shadowProps[input.dataset.type];
+      syncInputs(input.value, input, shadowInputs[idx + 1]);
     }
-    updateTextShadow();
-}
-
-const removeShadowLi = e => {
-    const shadowToRemove = e.target.closest('li');
-    let nextShadow = shadowToRemove.nextElementSibling;
-    
-    // If next el has nex-shadow class than update id, content, data-attr;
-    while( nextShadow.classList.contains('new-shadow') ) {
-        const shadowIdEl = nextShadow.querySelector('span[data-shnum]');
-        // Get shadow id number and decrease by 1;
-        const shadowIdNum = +shadowIdEl.id.split('-')[1] - 1;
-        shadowIdEl.id =  `shadow-${shadowIdNum}`;
-        shadowIdEl.textContent = shadowIdNum;
-        shadowIdEl.dataset.shnum = shadowIdNum;
-        nextShadow = nextShadow.nextElementSibling;
-    }
-    shadowToRemove.remove();
-    shadowCounter--;
-    
-}
+  });
+};
 
 const renderShadowLi = (hookId) => {
-    const shadowLi = document.createElement('li');
-    shadowLi.className = 'new-shadow active';
-    shadowLi.innerHTML = `
+  const shadowLi = document.createElement("li");
+  shadowLi.className = "new-shadow active";
+  shadowLi.innerHTML = `
         <span id="shadow-${shadowCounter}" data-shnum='${shadowCounter}'>${shadowCounter}</span>
         <span class="delete-shadow" id="delete-shadow">
             <i class="fas fa-times"></i>
         </span>
     `;
-    document.getElementById(hookId).insertBefore(shadowLi, addShadowBtn);
-    shadowLi.querySelector('#delete-shadow').addEventListener('click', removeShadowLi);
-    shadowCounter++;
-    return shadowLi
-}
 
+  const shadowObj = createShadowObject();
+
+  updateNewShadowInputs(shadowObj);
+
+  document.getElementById(hookId).insertBefore(shadowLi, addShadowBtn);
+  shadowLi
+    .querySelector("#delete-shadow")
+    .addEventListener("click", removeShadowLi);
+  shadowCounter++;
+  return shadowLi;
+};
+
+const getTextShadowString = (obj) => {
+  //let str = "";
+  // shadowInputs.forEach((input) => {
+  //   if (input.type === "range" && input.id !== "opacity") {
+  //     str += `${input.value}px `;
+  //   } else if (input.type === "color") {
+  //     str += `${hexToRgba(input.value, shadowOpacityInput.value)}`;
+  //   }
+  // });
+  const { hLen, vLen, blurRadius, opacity, shadowColor } = obj;
+  return `${hLen}px ${vLen}px ${blurRadius}px ${hexToRgba(
+    shadowColor,
+    opacity
+  )}`;
+  //console.log(str);
+};
+
+const updateTextShadow = () => {
+  //console.log(getTextShadowString(shadowObj));
+  let str = "";
+  shadows.forEach((shadow, idx) => {
+    const prefix = idx === shadows.length - 1 ? "" : ",";
+    str += getTextShadowString(shadow) + prefix;
+  });
+  //console.log(str);
+  previewTextEl.style.textShadow = str;
+  updateCode(str);
+  return str;
+};
+
+const updateCode = (str) => {
+  document.getElementById("code-string").textContent = `text-shadow: ${str};`;
+};
+
+// Handlers
+
+const textInputHandler = (e) => {
+  const value = e.target.value;
+  const type =
+    e.target.dataset.type === "text-color"
+      ? changePreviewText(value)
+      : changePreviewContainerBg(value);
+  syncInputs(value, textColorInput, textColorHexInput);
+};
 
 const addShadowHanlder = () => {
-    const shadowLi = renderShadowLi('shadow-list');;
-    if (shadowLi.previousElementSibling) {
-        shadowLi.previousElementSibling.classList.remove('active');
-    }
-}
+  const shadowLi = renderShadowLi("shadow-list");
+  if (shadowLi.previousElementSibling) {
+    shadowLi.previousElementSibling.classList.remove("active");
+  }
+  updateTextShadow();
+};
 
-
-
+const updateShadowHandler = (e) => {
+  const type = e.target.type;
+  const idx = document.querySelector(".shadows-list li.active > span").dataset
+    .shnum;
+  const shadow = shadows.find((el) => el.id === +idx);
+  if (type === "range") {
+    // Querying for range value preview el
+    const inputValuePreviewEl = e.target.previousElementSibling.querySelector(
+      "span"
+    );
+    syncInputAndText(e.target.value, inputValuePreviewEl);
+  } else if (type === "color") {
+    syncInputs(e.target.value, e.target, e.target.nextElementSibling);
+  } else if (type === "text") {
+    syncInputs(e.target.value, e.target, e.target.previousElementSibling);
+  }
+  shadow[e.target.dataset.type] = e.target.value;
+  updateTextShadow();
+};
 
 // Event Listeners
 
-textInputs.forEach(input => {
-    input.addEventListener('change', textInputHandler);
+textInputs.forEach((input) => {
+  input.addEventListener("change", textInputHandler);
 });
 
-shadowInputs.forEach(input => {
-    input.addEventListener('change', changeShadowHandler);
-    if (input.type === 'range') input.addEventListener('mousemove', changeShadowHandler);
-})
+shadowInputs.forEach((input) => {
+  input.addEventListener("change", updateShadowHandler);
+  if (input.type === "range")
+    input.addEventListener("mousemove", updateShadowHandler);
+});
 
-addShadowBtn.addEventListener('click', addShadowHanlder);
+addShadowBtn.addEventListener("click", addShadowHanlder);
